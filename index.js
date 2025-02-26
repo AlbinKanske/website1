615498e8adc2b939956e209bea8073a6f82a265f
AlbinKanske <166918249+AlbinKanske@users.noreply.github.com>
2025-02-26 22:39:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+const question = document.getElementById("questionBox");
+const answer = document.getElementById("answer");
+const subBtn = document.getElementById("subBtn");
+
+question.querySelector("label").textContent = "Randomly choosing a book can lead to surprising discoveries.";
